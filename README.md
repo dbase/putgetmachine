f899c3e2a8b826faf47c1b6fce5dcbf4b831bb4a
lancelion <emre@eunlu.com>
2014-02-02 00:29:02 +0200

    initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-a-http-client
-=============
-
-simple http client
--- a-http-server.c
+++
@@ -1,9 +0,0 @@
-# include <stdio.h>
-# include <stdlib.h>
-
-int main (int argc, char** argv)
-{
-fprintf("YOLO");
-fprintf("HOHO");
-return 0;
-}
